e67f0e1c9debbda7a81411c457b3e392bd7834ab
Gweru-Tech <Gwerutechnology@gmail.com>
2025-09-28 23:56:10 +0200

    Create Post.js

---
+++ models/Post.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true,
+    maxlength: 280
+  },
+  images: [{
+    type: String
+  }],
+  likes: [{
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  }],
+  comments: [{
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    text: String,
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Post', postSchema);
