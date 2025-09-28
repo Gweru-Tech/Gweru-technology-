ec3c472d9f9f1cce82e95ee48657d44374b9537f
Gweru-Tech <Gwerutechnology@gmail.com>
2025-09-28 23:55:36 +0200

    Create Product.js

---
+++ models/Product.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  business: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Business',
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  images: [{
+    type: String
+  }],
+  videos: [{
+    type: String
+  }],
+  category: {
+    type: String,
+    required: true
+  },
+  inStock: {
+    type: Boolean,
+    default: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Product', productSchema);
